7f7b973de380dedea62e2758e3f9a22bc6745c3b
Sumama-byte <100286760+Sumama-byte@users.noreply.github.com>
2022-09-16 12:55:50 +0500

    complete design first draft by sumama

--- src/app/create-activity/create-activity.page.ts
+++ src/app/create-activity/create-activity.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { LoadingController, MenuController, NavController } from '@ionic/angular';
 @Component({
   selector: 'app-create-activity',
   templateUrl: './create-activity.page.html',
@@ -8,12 +9,37 @@ import { Router } from '@angular/router';
 })
 export class CreateActivityPage implements OnInit {
 
-  constructor(public route :Router) { }
+  tabID = 1;
+
+  constructor(public route :Router ,   public loadingController: LoadingController) { }
 
   ngOnInit() {
   }
   
   go_back(){
     this.route.navigate(['/tabs/tab1']);
+    this.tabID = 1 ;
+  }
+
+  go_form_one(){
+    this.tabID = 1 ;
+  }
+
+  ShowHidePassword(ele, eye) {
+    if (ele.type === 'password') {
+      ele.type = 'text';
+      eye.name = 'eye-off-outline'
+    }
+    else {
+      ele.type = 'password';
+      eye.name = 'eye-outline'
+    }
+  }
+
+
+  presentLoading() {
+  console.log(this.tabID)
+  this.tabID = 2;
+  console.log(this.tabID)
   }
 }
