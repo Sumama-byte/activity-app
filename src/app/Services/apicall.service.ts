f7e6401cec9042158f457601dffa4ff89f38213c
Zagham Nadeem <zagham@learn2earnn.com>
2022-09-16 12:11:56 +0500

    Api's Services Created

---
+++ src/app/Services/apicall.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { GlobalService } from './global.service';
+import { HttpClient } from "@angular/common/http";
+@Injectable({
+  providedIn: 'root'
+})
+export class ApicallService {
+  login: any;
+  activity: any;
+
+  constructor( public global: GlobalService, public authservice: AuthService, public http: HttpClient ) { }
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  //Login
+  api_addLogin(login : any ){
+    this.authservice.con(login, 'login').then(async (res) => {
+      this.login = JSON.parse(String(res).toString());
+      // this.global.set_login(this.login);
+      if (this.login.error === false) {
+        console.log(this.login);
+        return;
+      }
+    }, (err) => {
+      console.log(err);
+    });
+  }
+  //Get Method
+  getActivities() {
+  return   this.authservice.getdata('getappointment').then((result) => {
+      this.activity = JSON.parse(String(result));
+      // this.global.set_activity(this.activity);
+      console.log(this.activity,'data Updated');
+      return result;
+    }, (err) => {
+      console.log(err);
+      return err
+    });
+  }
+}
