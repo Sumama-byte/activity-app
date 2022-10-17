e86a866a5d6738c93fd8dec4767b50d881da6dff
ranarehan0007 <ranarehan4386@gmail.com>
2022-10-17 19:34:10 +0500

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { environment } from '../environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireModule } from '@angular/fire/compat'
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig), 
-    AngularFireAuthModule,    
-      AngularFirestoreModule
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule
     ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
