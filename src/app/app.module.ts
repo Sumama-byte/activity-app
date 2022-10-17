d6e734e776ce764a360b814961d23c5703461585
Zagham Nadeem <zagham@learn2earnn.com>
2022-10-17 16:12:41 +0500

    app icon and splash icon added in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-// import { AngularFireModule } from '@angular/fire';
-// import { AngularFireAuthModule } from '@angular/fire/auth';
-// import { AngularFirestoreModule } from '@angular/fire/firestore';
-
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat'
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AngularFireAuthModule,    
+      AngularFirestoreModule
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -25,31 +25,7 @@ export class Tab1Page implements ViewDidEnter {
 
   public profile_data : any ={u_id:'', name:'',  img:'' , bio: '' , socialize_distance:'' };
   public Profile: any ;
-
-
-  public list_activities:any=[
-    {id:1 ,user_img:'../../assets/Rectangle 142.png', activity_title:'Beach Party',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'32,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'2' , start_time:'05' ,end_time:'03',attende_no:'43' },
-
-    {id:2 ,user_img:'../../assets/Rectangle 143.png', activity_title:'Swimming Together',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'31,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'23' , start_time:'05' ,end_time:'03',attende_no:'23'},
-
-    {id:3 ,user_img:'../../assets/Rectangle 144.png', activity_title:'Going For Excercise ',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'34,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'22' , start_time:'05' ,end_time:'03',attende_no:'12'},
-
-    {id:4 ,user_img:'../../assets/Rectangle 145.png', activity_title:'Beach Party',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'36,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'42' , start_time:'05' ,end_time:'03',attende_no:'23'},
-
-    {id:5 ,user_img:'../../assets/Rectangle 143.png', activity_title:'Swimming Together',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'38,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'32' , start_time:'05' ,end_time:'03',attende_no:'56'},
-
-    {id:6 ,user_img:'../../assets/Rectangle 144.png', activity_title:'Going For Excercise ',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'31,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'12' , start_time:'05' ,end_time:'03',attende_no:'45'},
-
-    {id:7 ,user_img:'../../assets/Rectangle 145.png', activity_title:'Beach Party',activity_des:'Lets swimming together near a beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .beach and play a volly ball with each other .'
-     , location_address:'30,street ,USA' , location_img:'../../assets/Rectangle 12942.png', location_range:'24' , start_time:'05' ,end_time:'03',attende_no:'43'},
-
-]
+  private userName:any;
   uid: any;
 
 
@@ -75,7 +51,8 @@ export class Tab1Page implements ViewDidEnter {
     console.log(this.uid);
     await this.global.ProfileInfo.subscribe(res => {
     console.log(res[0].img)
-   this.Profile= res[0].img;
+    this.Profile= res[0].img;
+    this.userName = res[0].name;
   });
    }
 
@@ -108,7 +85,7 @@ export class Tab1Page implements ViewDidEnter {
   await this.apiCall.api_getpeopleForChat();
   }
 
-  // navigations
+  // navigation
   notification(){
     this.getAllActivity();
     this.get_appData();
@@ -164,7 +141,7 @@ export class Tab1Page implements ViewDidEnter {
           lat: coordinates.coords.latitude,
           lng: coordinates.coords.longitude
         },
-        title:this.notificationsActivity.name,
+        title:this.userName,
         snippet:"Zagham",
         iconUrl: this.Profile,
         iconSize: {
@@ -175,11 +152,17 @@ export class Tab1Page implements ViewDidEnter {
     ])
     for(let i=0; i<this.notificationsActivity.length; i++) {
       console.log(this.notificationsActivity[i]);
+      console.log(`${this.notificationsActivity[i].lng}`);
+      const x = this.notificationsActivity[i].lng
+      const lat:number =  +x;
+      const y = this.notificationsActivity[i].lat
+      const lng = +y;
+      console.log(lat, lng)
       this.newMap.addMarkers([
         {
           coordinate:{
-            lat: parseInt(this.notificationsActivity[i].lat),
-            lng: parseInt(this.notificationsActivity[i].lng)
+            lat: lat,
+            lng: lng
           },
           title:this.notificationsActivity[i].name,
           snippet:"Zagham",
